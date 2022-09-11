fc50d1defc206cdab0af1a188057a6558dff2517
AlelaPetro <onguko37@gmail.com>
2022-09-11 12:02:12 -0700

    6-print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: single digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z' ; c >= 'a' ; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
