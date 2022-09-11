7aa75c91f3732b1410f55ae7bb8c003ddeb8fc10
AlelaPetro <onguko37@gmail.com>
2022-09-11 11:50:30 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Enty point
+ *
+ * Description: Prints numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 0; i++)
+	{
+		printf("%i" , i);
+	}
+	printf('\n');
+	return (0);
+}
