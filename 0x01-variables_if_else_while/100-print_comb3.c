9cd617616fef518ebab33b837e3d520f294e5bb7
AlelaPetro <onguko37@gmail.com>
2022-09-12 09:32:06 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,16 +14,21 @@ int main(void)
 	int b;
 
 	for (a = 0; a <= 9; a++)
+	{
 	for (b = 1; b <= 9; b++)
 	{
 		if (b > a);
-		putchar(a + '0');
-		putchar(b + '0');
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			if (a > b);
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
-	{
-		if (a > b);
-		putchar(',');
-		putchar(' ');
 	}
+}
 	putchar('\n');
 	return (0);
