693da0aec4b59a82ad1b927236c511bdf9dbc397
AlelaPetro <onguko37@gmail.com>
2022-09-12 09:46:57 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints numbers
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 1; b <= 9; b++)
+		{
+			for (c = 2; c <= 9; c++)
+			{
+				if (c > b && b > a)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+					if (a != 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
