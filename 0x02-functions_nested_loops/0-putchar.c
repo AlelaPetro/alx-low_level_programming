4a5f91d4df8a70be329b5c27a04c347639551894
AlelaPetro <onguko37@gmail.com>
2022-09-13 05:04:01 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main _ Entry point
+ *
+ * Description: prints _putchar
+ *
+ * Returns: 0 always
+ */
+int main(void)
+{
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar("\n");
+	return (0);
+}
