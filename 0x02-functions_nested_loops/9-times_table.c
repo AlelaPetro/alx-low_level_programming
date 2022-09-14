1ab672ac86310f431ea0f1a9b923064766357059
AlelaPetro <onguko37@gmail.com>
2022-09-14 08:43:56 -0700

    void times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,13 +4,14 @@
  *
  * Return: 0
  */
+
 void times_table(void);
 {
 	int num;
       	int mult;
        	int prod;
 
-	for (num = 0 ; num <= 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
 
