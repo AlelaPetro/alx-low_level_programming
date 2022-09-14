be40d8d03a97857cebbacb914f668884c0882796
AlelaPetro <onguko37@gmail.com>
2022-09-14 03:19:38 -0700

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,15 +15,15 @@ void jack_bauer(void)
 		while (hours <= 23)
 		{
 			while (minutes <= 59)
-				mins_reminder = minutes % 10;
-				hours_reminder = hours % 10;
-				_putchar(hours / 10 + '0');
-				_putchar(hours_reminder + '0')
-				_putchar(':');
-				_putchar(minutes / 10+ '0');
-				_putchar(mins_reminder + '0');
-				minutes++;
-				_putchar('\n');
+			mins_reminder = minutes % 10;
+			hours_reminder = hours % 10;
+			_putchar(hours / 10 + '0');
+			_putchar(hours_reminder + '0')
+			_putchar(':');
+			_putchar(minutes / 10+ '0');
+			_putchar(mins_reminder + '0');
+			minutes++;
+			_putchar('\n');
 		}
 		hours++;
 		minutes = 0;
