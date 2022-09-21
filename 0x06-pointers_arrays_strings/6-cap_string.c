abe2003be39f7cfdaf806f264ac9a978bfe2d1dc
AlelaPetro <onguko37@gmail.com>
2022-09-21 13:36:08 -0700

    6-cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _strlen - function returns length of the string
+ * @s: string
+ * Return: returns length of integer
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len+=;
+
+	return (len);
+}
+
+/**
+ * cap_string - function that capitalizes first letter of the word
+ * @str: string to capitalize
+ * Return: returns capitalized spring
+ */
+char *cap_string(char *)
+{
+	int index = 0;
+
+	while (str[++index])
+	{
+		while (!(str[index] >= 'a') && (str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}' )
+			str[index] -= 32;
+	}
+
+	return (str);
+}
