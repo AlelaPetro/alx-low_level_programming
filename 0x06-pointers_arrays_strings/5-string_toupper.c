a6a106075118a5cf91ed453cdb1e6050725a339a
AlelaPetro <onguko37@gmail.com>
2022-09-22 11:40:30 -0700

    5-string

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,13 +8,13 @@
  */
 char *string_toupper(char *)
 {
-	int i;
+	int index = 0;
 
-	while (str[i] != '\0')
+	while (str[index++])
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
-		i++;
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
 	}
+
 	return (str);
 }
