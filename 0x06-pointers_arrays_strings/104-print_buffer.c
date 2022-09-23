3bcd17a080c1ad1b62fb9b1a777b02ebbd2aad48
AlelaPetro <onguko37@gmail.com>
2022-09-22 20:17:21 -0700

    12b

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -25,21 +25,24 @@ void print_line(char *c, int s, int l)
 	for (k = 0; k <= s; k++)
 	{
 		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(',');
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
 	}
 }
+
 /**
  * print_buffer - prints buffer
  * @b: buffer to print
  * @size: size of buffer
  *
- * REturn: void
+ * Return: void
  */
 void print_buffer(char *b, int size)
 {
 	int i;
 
-	for (i = 0; i <= (size -1) / 10 && size; i++)
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
 		printf("%08x: ", i * 10);
 		if (i < size / 10)
@@ -50,7 +53,7 @@ void print_buffer(char *b, int size)
 		{
 			print_line(b, size % 10 - 1, i);
 		}
-		putchar('\n')
+		putchar('\n');
 	}
 	if (size == 0)
 		putchar('\n');
