a645e5d8805d126f56ae6a772c947a1ac4fdbcf5
AlelaPetro <onguko37@gmail.com>
2022-10-02 05:48:07 -0700

    3-mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,13 +14,13 @@ int main(int argc, char *argv[])
 	int result, num1, num2;
 
 	if (argc != 3)
+		printf("Error\n");
+	else
 	{
-		printf("%s\n", "Error");
-		return (1);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	result = num1 * num2;
-	printf("%d\n", result);
 	return (0);
 }
