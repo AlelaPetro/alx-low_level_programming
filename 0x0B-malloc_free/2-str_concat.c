f4e68d6640f7af81f50aa0c78bda543fd4d30b8c
AlelaPetro <onguko37@gmail>
2022-10-04 08:17:24 -0700

    2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,7 @@
  *
  * Return: NULL if failure otherwise pointer to string
  */
-char *str_concat(char *s1, char *s2);
+char *str_concat(char *s1, char *s2)
 {
 	char *concat_str;
 	int index, concat_index = 0, len = 0;
