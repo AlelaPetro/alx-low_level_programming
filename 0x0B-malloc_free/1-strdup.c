509efbe73dc5396319fc8dca05caaf502f1074e1
AlelaPetro <onguko37@gmail>
2022-10-04 06:49:44 -0700

    -strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strdup - returns pointer to allocated space in mem 
+ * @str: string to be cxopied
+ *
+ * Return: NULL if error otherwise to allocated pointer
+ */
+char *_strdup(char *str)
+{
+	char *cpy;
+	int index, len;
+
+	if (tr == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		len++
+			cpy = malloc(sizeof(char) * (len -1));
+
+	if (cpy == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+	{
+		cpy[index] = str[index];
+	}
+	
+	cpy[len] = '\0';
+
+	return (cpy);
+}
