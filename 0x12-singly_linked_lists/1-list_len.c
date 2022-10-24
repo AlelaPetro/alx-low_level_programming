bc438407b5811af9faf1551f6b975d27e44d1190
AlelaPetro <onguko37@gmail>
2022-10-24 11:24:14 -0700

    tsk1&2

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked list_t list
- * @h : pointer to the linked list
+ * list_len - finds the number of elements in a linked list
+ * @h: pointer to the start of the linked list
  *
- * Return: number of elements in a linked list
+ * Return: number of elements in the linked list
  */
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
+    const list_t *tmp;
+    unsigned int i;
 
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
+    tmp = h;
+    for (i = 0; tmp; i++)
+        tmp = tmp->next;
+    return (i);
 }
+
